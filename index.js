0e77b165197588e19ff2d1fbb9a8ace5dcc30e0c
Aaron <ztoplin@yahoo.com>
2022-09-13 14:41:24 -0400

    commit tree 040aa05f9bc9229e7cd4375edf58aeda89d142e3

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(shout(string));
+}
+
+function logWhisper(string) {
+    console.log(whisper(string));
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
